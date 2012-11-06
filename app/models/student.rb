56adc9c73adf0dd42ca308e369ce71e66fb26948
Apprentice <apprentice@devbootcamp.com>
2012-11-05 16:44:12 -0800

    validations executed and passed

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,7 +1,12 @@
 require_relative '../../db/config'
 
 class Student < ActiveRecord::Base
-  attr_accessible :first_name, :last_name, :gender, :birthday
+
+  validates :email, :format => { :with => /.+@.+\..{2,}/}, :uniqueness => true
+  validates :age, :numericality => { :only_integer => true, :greater_than_or_equal_to => 5 }
+  validates :phone, :format => { :with => /\(?\d{3}[\-\.\)\s]?\s?\d{3}[\-\.]?\d{4}\.?/}
+
+  attr_accessible :first_name, :last_name, :gender, :birthday, :email, :phone
 
   def name
     self.first_name + " " + self.last_name
--- db/migrate/20121011144238_create_students.rb
+++ db/migrate/20121011144238_create_students.rb
@@ -7,7 +7,9 @@ class CreateStudents < ActiveRecord::Migration
       t.string :first_name
       t.string :last_name
       t.string :gender
+      t.string :email
       t.date :birthday
+      t.string :phone
       t.timestamps
     end
   end
